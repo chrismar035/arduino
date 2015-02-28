551fce2ac7aea9afa67e4461924f41b77c228d39
Chris Marshall <chris@chrismar035.com>
2015-02-28 18:22:44 -0500

    Working LED switch

--- esp8266/led-switch/led-switch.ino
+++ esp8266/led-switch/led-switch.ino
@@ -4,113 +4,108 @@
 #define SSID ""
 #define PASS ""
 #define IP "192.168.1.75"
-SoftwareSerial monitor(10, 11); // RX, TX
+SoftwareSerial espy(10, 11); // RX, TX
 
-String GET = "GET /test";
+String COMMAND_ON = "ON";
+String COMMAND_OFF= "OFF";
 
-
-int redPin = 13;
-int greenPin = 12;
+int ledPin = 13;
+int indicatorPin = 5;
+bool connected = false;
 
 void setup() {
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
 
   // put your setup code here, to run once:
-  monitor.begin(115200);
-  monitor.println("AT");
+  Serial.begin(9600);
+  Serial.println("Starting");
+  espy.begin(9600);
+  espy.println("AT");
   delay(1000);
-  if(monitor.find("OK")){
-    digitalWrite(greenPin, HIGH);
-    connectWiFi();
-  } else {
-    digitalWrite(redPin, HIGH);
-  }
 
+  int maxTries = 5;
+  if(espy.find("OK")) {
+    while(!connected && maxTries > 0) {
+      Serial.println("Attempting to connect. Try " + String(maxTries));
+      connected = connectWiFi();
+      maxTries--;
+    }
+    if(connected) {
+      espy.println("AT+CWMODE=1");
+      delay(1000);
+      espy.println("AT+CIPMUX=1");
+      delay(1000);
+      espy.println("AT+CIPSERVER=1");
+      delay(1000);
+      Serial.println("Server started");
+    } else {
+      Serial.println("Unable to connect");
+    }
+  } else {
+    Serial.println("ESP not ready");
+  }
+  Serial.println("Getting IP");
+  espy.println("AT+CIFSR");
+  delay(2000);
+  Serial.println(espy.readString());
 }
 
 void loop() {
-  sendTest();
-  startBlink();
-  delay(3000);
-  // put your main code here, to run repeatedly:
-  /* digitalWrite(redPin, LOW); */
-  /* digitalWrite(greenPin, HIGH); */
-  /* delay(50); */
-  /* digitalWrite(redPin, HIGH); */
-  /* digitalWrite(greenPin, LOW); */
-  /* delay(50); */
-  /* digitalWrite(redPin, LOW); */
-  /* delay(100); */
-}
+  if (connected) {
+    digitalWrite(indicatorPin, HIGH);
 
-void sendTest(){
-  digitalWrite(redPin, LOW);
-  digitalWrite(greenPin, LOW);
-  String cmd = "AT+CIPSTART=\"TCP\",\"";
-  cmd += IP;
-  cmd += "\",3000";
-  monitor.println(cmd);
-  delay(2000);
-  if(monitor.find("Error")){
-    blinkRed();
-    return;
+    if (espy.find("led-on")) {
+      digitalWrite(ledPin, HIGH);
+    } else {
+      digitalWrite(ledPin, LOW);
+    }
   }
-  cmd = GET;
-  cmd += "\r\n";
-  monitor.print("AT+CIPSEND=");
-  monitor.println(cmd.length());
-  if(monitor.find(">")){
-    digitalWrite(greenPin, HIGH);
-    monitor.print(cmd);
-  }else{
-    digitalWrite(redPin, HIGH);
-    monitor.println("AT+CIPCLOSE");
-  }
+  delay(50);
 }
 
 boolean connectWiFi(){
-  startBlink();
-  monitor.println("AT+CWMODE=1");
+  Serial.println("Connecting to access point");
+  espy.println("AT+CWMODE=1");
   delay(2000);
   String cmd="AT+CWJAP=\"";
   cmd+=SSID;
   cmd+="\",\"";
   cmd+=PASS;
   cmd+="\"";
-  monitor.println(cmd);
-  startBlink();
+  espy.println(cmd);
+  Serial.println("Waiting for connection");
   delay(5000);
-  if(monitor.find("OK")){
-    digitalWrite(greenPin, HIGH);
+  if(espy.find("OK")){
+    Serial.println("Connected!");
     return true;
   }else{
-    digitalWrite(redPin, HIGH);
+    Serial.println("Unable to connect");
+    espy.println("AT+RST");
+    delay(1000);
     return false;
   }
 }
 
-void startBlink() {
-  digitalWrite(redPin, LOW);
-  digitalWrite(greenPin, HIGH);
-  delay(50);
-  digitalWrite(redPin, HIGH);
-  digitalWrite(greenPin, LOW);
-  delay(50);
-  digitalWrite(redPin, LOW);
-}
-
-void blinkRed() {
-  digitalWrite(greenPin, LOW);
-  digitalWrite(redPin, HIGH);
-  delay(50);
-  digitalWrite(redPin, LOW);
-  delay(50);
-  digitalWrite(redPin, HIGH);
-  delay(50);
-  digitalWrite(redPin, LOW);
-  delay(50);
-  digitalWrite(redPin, HIGH);
-  delay(50);
-  digitalWrite(redPin, LOW);
-}
+/* void sendCommand(String command){ */
+/*   Serial.println("Sending command: " + command); */
+/*   String cmd = "AT+CIPSTART=\"TCP\",\""; */
+/*   cmd += IP; */
+/*   cmd += "\",3000"; */
+/*   espy.println(cmd); */
+/*   Serial.println("Waiting for command connection to server"); */
+/*   delay(2000); */
+/*   if(espy.find("Error")){ */
+/*     return; */
+/*   } */
+/*   cmd = command; */
+/*   cmd += "\r\n"; */
+/*   espy.print("AT+CIPSEND="); */
+/*   espy.println(cmd.length()); */
+/*   if(espy.find(">")){ */
+/*     espy.print(cmd); */
+/*     Serial.println("Command sent"); */
+/*   }else{ */
+/*     Serial.println("Command error; closing connection"); */
+/*     espy.println("AT+CIPCLOSE"); */
+/*   } */
+/* } */
